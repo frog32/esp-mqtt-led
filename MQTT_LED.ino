dc4645064321be9186581e7adf5826a68892a75c
Marc Egli <frog32@me.com>
2018-01-09 23:01:56 +0100

    change to IOT appstory

--- MQTT_LED.ino
+++ MQTT_LED.ino
@@ -1,53 +1,31 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-//#include <DNSServer.h>
-#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+#define MQTT_SOCKET_TIMEOUT 0
 #include <PubSubClient.h>
-#include <EEPROM.h>
-#include <DoubleResetDetector.h>
-#include "FastLED.h"
-#include <Ticker.h>
+#define FASTLED_INTERRUPT_RETRY_COUNT 0
+#include <FastLED.h>
 #include <ArduinoJson.h>
 
 #include <string.h>
 
+#define APPNAME "MQTT LED"
+#define VERSION "V0.0.1"
+// #define COMPDATE __DATE__ __TIME__
+#define COMPDATE __DATE__
+#define MODEBUTTON 0
 
 
-#define DATA_PIN 5
+#include <IOTAppStory.h>
+IOTAppStory IAS(APPNAME, VERSION, COMPDATE, MODEBUTTON);
 
-
-struct MQTT_LED_config {
-  unsigned char config_version;
-  char broker[40];
-  char port[6] = "1883";
-  char user[20];
-  char password[20];
-  char prefix[40];
-  unsigned short num_leds = 10;
-} MQTT_LED_config;
-
-#define CONFIG_VERSION 1
-
-#define DRD_TIMEOUT 10
-
-#define DRD_ADDRESS 0
-
-DoubleResetDetector drd(DRD_TIMEOUT, DRD_ADDRESS);
+// switch this to 5 if using generic board
+#define DATA_PIN 1
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-WiFiManager wifiManager;
-
-WiFiManagerParameter cfg_mqtt_broker("server", "mqtt broker", "", 40);
-WiFiManagerParameter cfg_mqtt_port("port", "mqtt port", "", 6);
-WiFiManagerParameter cfg_mqtt_user("user", "mqtt user", "", 20);
-WiFiManagerParameter cfg_mqtt_password("password", "mqtt password", "", 20);
-WiFiManagerParameter cfg_mqtt_prefix("prefix", "mqtt prefix", "", 40);
 
 // led stuff
 CRGB* leds;
-
-Ticker LEDcontroll;
 
 #define LIGHT_OFF 0
 #define LIGHT_ON 1
@@ -57,23 +35,32 @@ Ticker LEDcontroll;
 #define EFFECT_COLORLOOP 2
 #define EFFECT_GRADIENT 3
 #define EFFECT_JUNGLE 4
-#define EFFECT_CONFETTI 5
-#define EFFECT_LIGHTNING 6
+#define EFFECT_JUNGLE2 5
+#define EFFECT_CONFETTI 6
+#define EFFECT_LIGHTNING 7
+#define EFFECT_TWINKLE 8
+#define EFFECT_TWINKLE2 9
 
-const char effectMap[7][10] = {
+const char effectMap[10][10] = {
  "solid",
  "rainbow",
  "colorloop",
- "gradient", // todo: implement
+ "gradient",
  "jungle",
+ "jungle2",
  "confetti",
- "lightning",
+ "lightning", // todo: implement
+ "twinkle",
+ "twinkle2",
 };
 
 struct light {
   unsigned char state = LIGHT_OFF;
   unsigned char effect = EFFECT_SOLID;
+  unsigned short transition = 0;
   unsigned char brightness = 255;
+  unsigned char speed = 100;
+  unsigned char gain = 100;
   unsigned char red = 255;
   unsigned char green = 255;
   unsigned char blue = 255;
@@ -85,88 +72,140 @@ struct light {
 char topic_buffer[60];
 
 
+struct Config {
+  char* broker = "1.1.1.1";
+  char* port = "1883";
+  char* user = "user";
+  char* password = "password";
+//  char root[3000] = "";
+  char* prefix = "test";
+  char* numLedsRaw = "16";
+  unsigned short numLeds;
+  char* colorOrder = "RGB";
+  char* powerRaw = "1500";
+  unsigned short power;
+} Config;
+
+unsigned long lastReconnectAttempt = 0;
+unsigned long lastLedUpdate = 0;
 
 
 //
 void setup() {
-  Serial.begin(115200);
+  ESP.wdtDisable();
+  ESP.wdtEnable(5500);
 
-  EEPROM.begin(sizeof(MQTT_LED_config));
-  bool hasValidConfig = loadMQTTConfig();
-  bool isDoubleReset = drd.detectDoubleReset();
+  IAS.serialdebug(true);
+  strcpy(IAS.config.IOTappStory1, "static.frog32.ch");
+  strcpy(IAS.config.IOTappStoryPHP1, "/ota/mqtt_led.bin");
+  strcpy(IAS.config.IOTappStory2, "static.frog32.ch");
+  strcpy(IAS.config.IOTappStoryPHP2, "/ota/mqtt_led.bin");
 
-  if (isDoubleReset || !hasValidConfig) {
-    drd.stop();
-    if(isDoubleReset)
-      Serial.println("Double Reset Detected");
-    if(!hasValidConfig)
-      Serial.println("invalid config");
-    Serial.println("Entering config mode");
-    startConfig();
-  }
+  IAS.addField(Config.broker, "mqtt_broker", "MQTT Broker IP", 39);
+  IAS.addField(Config.port, "mqtt_port", "Port", 5);
+//  IAS.addField(Config.root, "TLS", "mqtt_root", 2999);
+  IAS.addField(Config.user, "mqtt_user", "User", 19);
+  IAS.addField(Config.password, "mqtt_password", "Password", 19);
+  IAS.addField(Config.prefix, "mqtt_prefix", "Prefix", 39);
+  IAS.addField(Config.numLedsRaw, "num_leds", "Num LEDs", 3);
+  IAS.addField(Config.colorOrder, "color_order", "Color Order", 3);
+  IAS.addField(Config.powerRaw, "power", "LED Power (mW)", 5);
 
+  IAS.begin(true, false);  // todo: check if erase all is needed once released
 
   // init leds
+  Config.numLeds = atoi(Config.numLedsRaw);
   Serial.print("Reserve space for LEDs ");
-  Serial.println(MQTT_LED_config.num_leds);
-  leds = (CRGB*)malloc(sizeof(CRGB) * MQTT_LED_config.num_leds);
+  Serial.println(Config.numLeds);
+  leds = (CRGB*)malloc(sizeof(CRGB) * Config.numLeds);
   yield();
   Serial.println("Create LEDs");
-  LEDS.addLeds<WS2811, DATA_PIN, GRB>(leds, MQTT_LED_config.num_leds);
+  if(strcmp(Config.colorOrder, "BRG") == 0) {
+    LEDS.addLeds<WS2811, DATA_PIN, BRG>(leds, Config.numLeds);
+  } else if(strcmp(Config.colorOrder, "GRB") == 0) {
+    LEDS.addLeds<WS2811, DATA_PIN, GRB>(leds, Config.numLeds);
+  } else {
+    LEDS.addLeds<WS2811, DATA_PIN, RGB>(leds, Config.numLeds);    
+  }
   yield();
-  FastLED.setMaxPowerInVoltsAndMilliamps(5, 300);  // todo: move to settings
+  Config.power = atoi(Config.powerRaw);
+  FastLED.setMaxPowerInMilliWatts(Config.power);
 
   client.setCallback(mqttCallback);
 
-  LEDcontroll.attach(0.02, LEDupdate);
-
-  delay(5000);
-  drd.stop();
   Serial.println("Setup complete");
 }
 
 void loop() {
-
-  if (!client.connected()) {
+  IAS.buttonLoop();
+  LEDupdate();
+  if (client.connected()) {
+      client.loop();
+  } else if (millis() > lastReconnectAttempt + 20000 || millis() < lastReconnectAttempt) {
     mqttReconnect();
+    lastReconnectAttempt = millis();
   }
-  client.loop();
-
-  yield();
+  delay(2);
 }
 
-static uint8_t hue = 0;
+static uint8_t hue = 0, loopcounter = 0;
 
 void LEDupdate(void) {
-  int i;  // used in some effects
+  if (millis() < lastLedUpdate + 20 && millis() > lastLedUpdate) {  // todo: make update interval changable
+    return;
+  }
+  lastLedUpdate = millis();
+  int i, j;  // used in some effects
+  CRGB color1 = CRGB(light.red, light.green, light.blue);
+  CRGB color2 = CRGB(light.red2, light.green2, light.blue2);
   if(light.state == LIGHT_OFF) {
-     fill_solid(leds, MQTT_LED_config.num_leds, CRGB::Black);
+    fadeToBlackBy( leds, Config.numLeds, 20);
   } else {
     switch (light.state ? light.effect : LIGHT_OFF)
     {
       case EFFECT_SOLID:
-        fill_solid(leds, MQTT_LED_config.num_leds, CRGB(light.red, light.green, light.blue));  // todo: refactor CRGB(light.red, light.green, light.blue)
+        fill_solid(leds, Config.numLeds, color1);
         break;
       case EFFECT_RAINBOW:
-        // First slide the led in one direction
-        fill_rainbow(leds, MQTT_LED_config.num_leds, hue++ * 5, 255 / MQTT_LED_config.num_leds);
+        fill_rainbow(leds, Config.numLeds, hue, light.gain / 5);
+        if(loopcounter >= (255 - light.speed) % 128) {
+          hue += light.speed / 128 + 1;
+          loopcounter = 0;
+        }
         break;
       case EFFECT_COLORLOOP:
-        fill_solid(leds, MQTT_LED_config.num_leds, CHSV(hue++ * 5, 255, 255));
+        fill_solid(leds, Config.numLeds, CHSV(hue, 255, 255));
+        if(loopcounter >= (255 - light.speed) % 128) {
+          hue += light.speed / 128 + 1;
+          loopcounter = 0;
+        }
         break;
       case EFFECT_GRADIENT:
-  //      fill_gradient(leds, 0, CRGB(light.red, light.green, light.blue), MQTT_LED_config.num_leds, CRGB(light.red2, light.green2, light.blue2), SHORTEST_HUES);
+        fill_gradient_RGB(leds, 0, color1, Config.numLeds, color2);
         break;
       case EFFECT_JUNGLE:
-        fadeToBlackBy( leds, MQTT_LED_config.num_leds, 20);
-        for ( i = 0; i < MQTT_LED_config.num_leds; i = i + 10) {
-          leds[beatsin16(i + 7, 0, MQTT_LED_config.num_leds)] |= CRGB(light.red, light.green, light.blue);
+      case EFFECT_JUNGLE2:
+        fadeToBlackBy( leds, Config.numLeds, light.speed / 10 + 1);
+        for ( i = 0; i < light.gain; i = i + 16) {
+          if(light.effect == EFFECT_JUNGLE || i % 2)
+            leds[beatsin16(light.speed / 10 + i, 0, Config.numLeds - 1)] |= color1;
+          else
+            leds[beatsin16(light.speed / 10 + i, 0, Config.numLeds - 1)] |= color2;
         }
         break;
       case EFFECT_CONFETTI:
-        fadeToBlackBy( leds, MQTT_LED_config.num_leds, 20);
-        i = random16(MQTT_LED_config.num_leds);
-        leds[i] += CRGB(random8(64), random8(64), random8(64));
+      case EFFECT_TWINKLE:
+      case EFFECT_TWINKLE2:
+        fadeToBlackBy( leds, Config.numLeds, light.speed / 10 + 1);
+        for(i = 0; i < 3 && random8() < light.gain; i++) {
+          if(light.effect == EFFECT_CONFETTI)
+            leds[random16(Config.numLeds)] += CRGB(random8(64), random8(64), random8(64));
+          else if(light.effect == EFFECT_TWINKLE || random(2))
+            leds[random16(Config.numLeds)] += color1;
+          else
+            leds[random16(Config.numLeds)] += color2;
+        }
+        
         break;
       default:
         Serial.println("non valid effect");
@@ -174,23 +213,16 @@ void LEDupdate(void) {
   }
   FastLED.setBrightness(light.brightness);
   FastLED.show();
+  loopcounter++;
 }
 
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
-  unsigned char prefix_lenght = strlen(MQTT_LED_config.prefix);
+  unsigned char prefix_lenght = strlen(Config.prefix);
   char message_buffer[12];  // used for numeric to int conversion
   int i;
   Serial.println(topic);
-  if (strcmp (topic + prefix_lenght, "/num_leds/set") == 0) {  // todo: move to settings
-    Serial.println("num_leds");
-    memcpy(message_buffer, payload, _min(3, length));
-    message_buffer[_min(length, 11)] = '\0';
-    MQTT_LED_config.num_leds = (unsigned short)atoi(message_buffer);
-    saveMQTTConfig();
-  }
-
-  else if(strcmp (topic + prefix_lenght, "/set") == 0) {
+  if(strcmp (topic + prefix_lenght, "/set") == 0) {
     parseJSONCommand(payload, length);
     sendJSONStatus();
   }
@@ -213,6 +245,15 @@ void parseJSONCommand(byte* payload, unsigned int length) {
   }
   if(input.containsKey("brightness") && input["brightness"].is<int>()) {
     light.brightness = input["brightness"];
+  }
+  if(input.containsKey("transition") && input["transition"].is<int>()) {
+    light.transition = input["transition"];
+  }
+  if(input.containsKey("speed") && input["speed"].is<int>()) {
+    light.speed = input["speed"];
+  }
+  if(input.containsKey("gain") && input["gain"].is<int>()) {
+    light.gain = input["gain"];
   }
   if(input.containsKey("color") && input["color"].is<JsonObject>() && input["color"]["r"].is<int>()
       && input["color"]["g"].is<int>() && input["color"]["b"].is<int>()) {
@@ -257,125 +298,63 @@ void sendJSONStatus() {
 
 }
 
-void mqttReconnect() {
-  unsigned int attempt = 0;
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    if (attempt++ > 0) {
-      Serial.println("try again in 5 seconds");
-      delay(5000);
-    }
+bool mqttReconnect() {
+  Serial.println("Connect");
 
+//  WiFi.mode(WIFI_STA);
+//
+//  if (WiFi.status() != WL_CONNECTED) {
+//    ETS_UART_INTR_DISABLE();
+////      wifi_station_disconnect();
+//    ETS_UART_INTR_ENABLE();
+//    delay(1000);
+//
+//    if (WiFi.begin() != WL_CONNECTED) {
+//      Serial.println("WiFi connection failed");
+//      return(false);  // try again
+//    }
+//  }
 
-    Serial.println("Connect");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 
-    WiFi.mode(WIFI_STA);
+  Serial.print("Attempting MQTT connection to ");
+  Serial.print(Config.broker);
+  client.setServer(Config.broker, 1883);  // todo: use port from config
+  // Create a unique client ID
+  char clientId[13];
+  sprintf(clientId, "MQTT_LED%4X", ESP.getChipId());
+  // Attempt to connect
+  if (!client.connect(clientId, Config.user, Config.password, buildTopic("/status"),0 ,false, "{\"state\": \"OFF\"}")) {
+    Serial.print("failed, rc=");
+    Serial.println(client.state());
 
-    if (WiFi.status() != WL_CONNECTED) {
-      ETS_UART_INTR_DISABLE();
-      wifi_station_disconnect();
-      ETS_UART_INTR_ENABLE();
-      delay(1000);
-
-      if (WiFi.begin() != WL_CONNECTED) {
-        Serial.println("WiFi connection failed");
-        continue;  // try again
-      }
-    }
-
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-
-    Serial.print("Attempting MQTT connection to ");
-    Serial.print(MQTT_LED_config.broker);
-    client.setServer(MQTT_LED_config.broker, 1883);
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (!client.connect(clientId.c_str(), MQTT_LED_config.user, MQTT_LED_config.password, buildTopic("/light/status"),0 ,false, "OFF")) {
-      Serial.print("failed, rc=");
-      Serial.println(client.state());
-
-      continue;
-    }
-    Serial.println("connected");
-    // Once connected, publish an announcement...
-    client.publish(buildTopic("/light/status"), "OFF");
-    client.publish(buildTopic("/rgb/status"), "255,255,255");
-    // ... and resubscribe
-    char topic[sizeof(MQTT_LED_config.prefix) + 2];
-    sprintf(topic, "%s/#", MQTT_LED_config.prefix);
-
-    client.subscribe(topic);
-    Serial.print("Subscribed to ");
-    Serial.println(topic);
+    return(false);
   }
+  Serial.println("connected");
+  yield();
+  // Once connected, publish an announcement...
+  sendJSONStatus();
+  // ... and resubscribe
+  char topic[sizeof(Config.prefix) + 2];
+  sprintf(topic, "%s/#", Config.prefix);
+  yield();
+  client.subscribe(topic);
+  Serial.println("Subscribed to topic");
+//  Serial.println(topic);
+  return(true);
 }
 
 char* buildTopic(const char* suffix) {
-  strcpy(topic_buffer, MQTT_LED_config.prefix);
+  strcpy(topic_buffer, Config.prefix);
   strcat(topic_buffer, suffix);
   return topic_buffer;
 }
 
-bool loadMQTTConfig(void) {
-  Serial.println("load config");
-  for (unsigned int t = 0; t < sizeof(MQTT_LED_config); t++) {
-    *((char*)&MQTT_LED_config + t) = EEPROM.read(t);
-  }
-  if(MQTT_LED_config.num_leds > 1000) {  // prevent too large numbers
+bool sanitizeConfig(void) {
+  if(Config.numLeds > 1000) {  // prevent too large numbers
     Serial.println("Setting leds to 1000");
-    MQTT_LED_config.num_leds = 1000;
+    Config.numLeds = 1000;
   }
-  if (MQTT_LED_config.config_version == CONFIG_VERSION)
-    return true;
-  Serial.println("invalid configuration resetting it to 0");
-  Serial.println(MQTT_LED_config.config_version);
-  Serial.println(MQTT_LED_config.broker);
-
-  for (unsigned int t = 0; t < sizeof(MQTT_LED_config); t++)
-    *((char*)&MQTT_LED_config + t) = '0';
-  return false;
-}
-
-void extractWiFiManagerConfig(void) {
-  Serial.println("extract config");
-  strcpy(MQTT_LED_config.broker, cfg_mqtt_broker.getValue());
-  strcpy(MQTT_LED_config.port, cfg_mqtt_port.getValue());
-  strcpy(MQTT_LED_config.user, cfg_mqtt_user.getValue());
-  strcpy(MQTT_LED_config.password, cfg_mqtt_password.getValue());
-  strcpy(MQTT_LED_config.prefix, cfg_mqtt_prefix.getValue());
-  saveMQTTConfig();
-}
-
-void saveMQTTConfig(void) {
-  Serial.println("save config");
-  MQTT_LED_config.config_version = CONFIG_VERSION;
-  for (unsigned int t = 0; t < sizeof(MQTT_LED_config); t++)
-    EEPROM.write(t, *((char*)&MQTT_LED_config + t));
-  EEPROM.commit();
-}
-
-void startConfig(void) {
-  drd.stop();
-  // add custom parameter
-  wifiManager.addParameter(&cfg_mqtt_broker);
-  wifiManager.addParameter(&cfg_mqtt_port);
-  wifiManager.addParameter(&cfg_mqtt_user);
-  wifiManager.addParameter(&cfg_mqtt_password);
-  wifiManager.addParameter(&cfg_mqtt_prefix);
-
-  wifiManager.setSaveConfigCallback(extractWiFiManagerConfig);
-  strncpy(MQTT_LED_config.broker, cfg_mqtt_broker.getValue(), sizeof(MQTT_LED_config.broker));
-  strncpy(MQTT_LED_config.port, cfg_mqtt_port.getValue(), sizeof(MQTT_LED_config.port));
-  strncpy(MQTT_LED_config.user, cfg_mqtt_user.getValue(), sizeof(MQTT_LED_config.user));
-  strncpy(MQTT_LED_config.password, cfg_mqtt_password.getValue(), sizeof(MQTT_LED_config.password));
-  strncpy(MQTT_LED_config.prefix, cfg_mqtt_prefix.getValue(), sizeof(MQTT_LED_config.prefix));
-  WiFi.disconnect();
-  WiFi.mode(WIFI_AP_STA);
-  wifiManager.startConfigPortal("ondemandap", NULL);
-
 }
 
